2586c1821519a6b4d5cb1f4006381bc89880881b
conkraw <conkraw@gmail.com>
2024-10-24 13:50:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -580,9 +580,11 @@ elif st.session_state.page == "Method Details":
         with cols[1]:
             st.markdown("")
             st.markdown("")
+            st.markdown("")
             liter_flow = st.text_input("", key="liter_flow")
 
         with cols[2]:
+            st.markdown("")
             st.markdown("")
             st.markdown("")
             fio2 = st.text_input("", key="fio2")
