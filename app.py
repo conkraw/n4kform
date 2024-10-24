0f83e439b320256e72249a4dd8c9d5ecef65560a
conkraw <conkraw@gmail.com>
2024-10-24 13:11:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -411,8 +411,8 @@ elif st.session_state.page == "Course Information":
         cols = st.columns([4, 1])
         
         with cols[0]:
-            st.markdown("")  # Add spacing before the question
-            st.markdown("") 
+            #st.markdown("")  # Add spacing before the question
+            #st.markdown("") 
             #st.write(f"{idx + 1}. {question}")
             question_box(f"{idx + 1}. {question}")
             
