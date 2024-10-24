7900ddea3f0fce8c284526f9099ab66ab5096bac
conkraw <conkraw@gmail.com>
2024-10-24 12:30:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import threading
 
 st.set_page_config(layout="wide")
 
-def reset_input(default_value, key, width="100%", height="35px"):
+def reset_input(default_value, key, width="100%", height="50px"):
     # Add custom CSS for input styling
     st.markdown(
         f"""
