89f825ae2fde6175289cfc7750acbc9f73bb64a7
conkraw <conkraw@gmail.com>
2024-10-25 20:54:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -623,8 +623,9 @@ elif st.session_state.page == "Method":
             st.rerun()
 
 # Main application logic based on the current page
-if st.session_state.page == "Method Details":
+elif st.session_state.page == "Method Details":
     st.header("METHOD DETAILS")
+    
     if "selected_oxygen" not in st.session_state:
         st.session_state.selected_oxygen = "Select if Oxygen was Provided DURING any TI attempts for this course"
     if "oxygen_explanation" not in st.session_state:
