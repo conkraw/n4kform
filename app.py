0bb55e9e86d566da09850027ef9ca84929a01a46
conkraw <conkraw@gmail.com>
2024-11-08 12:16:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def create_word_doc(template_path, data):
 
     # Define your placeholders
     placeholders = {
-        '<<date_placeholder>>': data['date'],
+        '{{date_placeholder}}': data['date'],
         '<<time_placeholder>>': data['time'],
         '<<location_placeholder>>': data['location'],
         '<<sex_placeholder>>': data['patient_gender'],
