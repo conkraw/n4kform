6e0ce3f7f04880bceaff910b77455218c68b74b9
conkraw <conkraw@gmail.com>
2024-11-08 15:25:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -633,7 +633,7 @@ if st.session_state.page == "Summary":
                 'diagnostic_category':st.session_state.form_data['diagnostic_category'],
             }
             st.write(st.session_state.form_data['date'])
-            template_path = 'ntq.docx' 
+            template_path = 'nqf.docx' 
 
             try:
                 st.session_state.doc_file = create_word_doc(template_path, document_data)
